a1212fe95001cc56d3aea489e749e848cb3699fe
liuhuac <chenliuhuazju@gmail.com>
2015-05-07 16:05:20 -0400

    Update config.php

--- G1MeTube/config.php
+++ G1MeTube/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $dbhost = "mysql1.cs.clemson.edu";
-$dbuser = "mtbprjct_80tr";
-$dbpass = "1liuhua2zhuozhao";
-$database = "metubeproject_2squ";
+$dbuser = "username";
+$dbpass = "password";
+$database = "dbname";
 ?>
